be94902e264543a3146ab618433d9d6aadd63fa8
Enes Hajredini <68920977+EnesHajredini@users.noreply.github.com>
2025-09-27 15:10:53 +0200

    Delete Scripts directory

--- Scripts/Parallax.js
+++
@@ -1,6 +0,0 @@
-const parallax = document.querySelector('.parallax-div')
-
-window.addEventListener("scroll", () => {
-  let offset = window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.6 + "px";
-})
